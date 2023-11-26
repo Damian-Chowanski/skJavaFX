a6110d17cddca777502a1fcc7687e6fa2094cae5
Damian-Chowanski <dchowanski30@gmail.com>
2023-11-26 20:52:44 +0100

    created first view

--- src/main/java/com/example/erpjavafx/Main.java
+++ src/main/java/com/example/erpjavafx/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
