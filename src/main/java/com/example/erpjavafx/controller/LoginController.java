6314a966b8793b7fca09fcf0ec935c60e2f9e971
Damian-Chowanski <dchowanski30@gmail.com>
2023-11-28 20:33:43 +0100

    login controller created

---
+++ src/main/java/com/example/erpjavafx/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.erpjavafx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private Button logInBtn;
+
+}
