5bbf83c30d962c06bab3a4bb1c869e824e3b4265
Damian-Chowanski <dchowanski30@gmail.com>
2023-11-30 21:25:32 +0100

    added styles to progres bar

--- src/main/java/com/example/erpjavafx/factory/PopupFactory.java
+++ src/main/java/com/example/erpjavafx/factory/PopupFactory.java
@@ -15,13 +15,23 @@ public class PopupFactory {
         Stage stage = new Stage();
         stage.initStyle(StageStyle.UNDECORATED);
         VBox pane = new VBox();
+        pane.setStyle(waitingPopupPaneStyle());
         pane.setAlignment(Pos.CENTER);
         pane.setSpacing(10);
         Label label = new Label(text);
+        label.setStyle(waitingLabelStyle());
         ProgressBar progressBar = new ProgressBar();
         pane.getChildren().addAll(label, progressBar);
         stage.setScene(new Scene(pane, 200, 100));
         stage.initModality(Modality.APPLICATION_MODAL);
         return stage;
+    }
+
+    private String waitingLabelStyle() {
+        return "-fx-text-fill: #003366";
+    }
+
+    private String waitingPopupPaneStyle() {
+        return "-fx-background-color: #c7c7c7; -fx-border-color: #003366;";
     }
 }
